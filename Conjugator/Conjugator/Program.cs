fe93cc9b6bde76a62c37a246444438efce1f034c
David Sackstein <davids@codeprecise.com>
2020-10-11 23:20:17 +0300

    initial

---
+++ Conjugator/Conjugator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Conjugator
+{
+    class Program
+    {
+        const string nodeModulesPath = @"..\..\..\..\..\node_modules";
+        static void Main(string[] args)
+        {
+            string verbsFileName = Path.Combine(nodeModulesPath, @"french-verbs-list\verbs.json");
+            string frenchVerbsLefffName = Path.Combine(nodeModulesPath, @"french-verbs-lefff\dist\conjugations.json");
+            var t = File.Exists(frenchVerbsLefffName);
+            Console.WriteLine(t);
+        }
+    }
+}
