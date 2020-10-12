93d4d34f16ba27203089a3ab50a8ed0f7a7d5ce8
David Sackstein <davids@codeprecise.com>
2020-10-12 22:20:54 +0300

    Added test for present form of all er verbs

--- Conjugator/ConjugatorTests/UnitTest1.cs
+++ Conjugator/ConjugatorTests/UnitTest1.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using System.Linq;
 using ConjugatorLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -8,21 +8,46 @@ namespace ConjugatorTests
     [TestClass]
     public class UnitTest1
     {
-        private const string nodeModulesPath = @"..\..\..\..\..\node_modules";
-        private static VerbData verbData;
+        private const string _nodeModulesPath = @"..\..\..\..\..\node_modules";
+        private static VerbData _verbData;
+        private static Conjugator _conjugator;
 
         [ClassInitialize]
         public static void SetUp(TestContext context)
         {
-            verbData = new VerbData(nodeModulesPath);
+            _verbData = new VerbData(_nodeModulesPath);
+            _conjugator = new Conjugator();
         }
 
         [TestMethod]
-        public void TestMethod2()
+        public void TestMangerPresent()
         {
             string verb = "manger";
+            TestErPresent(verb);
+        }
 
-            Conjugation conjugation = verbData.Conjugations[verb];
+        [TestMethod]
+        public void TestAllErPresent()
+        {
+            Dictionary<bool, List<string>> grades = _verbData.Conjugations.Keys
+                .Where(v => v.EndsWith("er"))
+                .GroupBy(IsCorrect)
+                .ToDictionary(g => g.Key, g => g.ToList());
+
+            Assert.IsTrue(grades[true].Count >= 188);
+        }
+
+        private static bool IsCorrect(string verb)
+        {
+            Conjugation conjugation = _verbData.Conjugations[verb];
+            string[] expected = conjugation.Present;
+            string[] actual = _conjugator.GetErPresent(verb);
+            return expected != null && expected.SequenceEqual(actual);
+        }
+
+        private static void TestErPresent(string verb)
+        {
+            Conjugation conjugation = _verbData.Conjugations[verb];
             string[] expected = conjugation.Present;
 
             string[] actual = new Conjugator().GetErPresent(verb);
