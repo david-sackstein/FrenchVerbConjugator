c7e0a60e0bbe6dbbbc677ef9a951d050a63c9ec7
David Sackstein <davids@codeprecise.com>
2020-10-14 13:24:31 +0300

    autocélébrer - 61 errors left

--- Conjugator/ConjugatorLibrary/Conjugator.cs
+++ Conjugator/ConjugatorLibrary/Conjugator.cs
@@ -81,7 +81,7 @@ namespace ConjugatorLibrary
         {
             string stem = verb.Remove(verb.Length - 2);
 
-            if (verb == "appuyer")
+            if (verb == "autocélébrer")
             {
                 Console.WriteLine();
             }
@@ -98,7 +98,7 @@ namespace ConjugatorLibrary
             }
 
             string substring = verb.Substring(verb.Length-4, 2);
-            if (substring == "ch" || substring == "gu")
+            if (substring == "ch" || substring == "gu" || substring == "br")
             {
                 int replacedCharIndex = verb.Length - 5;
                 if (replacedCharIndex > 0 && stem[replacedCharIndex] == 'é')
--- Conjugator/ConjugatorTests/UnitTest1.cs
+++ Conjugator/ConjugatorTests/UnitTest1.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
 using ConjugatorLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
