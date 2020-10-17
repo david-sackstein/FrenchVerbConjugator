bfda5c1e68dc77ff25324e12dffe90118c383275
David Sackstein <davids@codeprecise.com>
2020-10-17 23:26:34 +0300

    ref 1

---
+++ Conjugator/ConjugatorLibrary/ConjugationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ConjugatorLibrary
+{
+    public static class ConjugationExtensions
+    {
+        public static string[] ForNonNousVous(this string[] conjugations, Func<string, string> others)
+        {
+            return conjugations
+                .Select((s, i) => IsNousVous(i)
+                    ? s
+                    : others(s))
+                .ToArray();
+        }
+
+        private static bool IsNousVous(int index)
+        {
+            return index == 3 || index == 4;
+        }
+    }
+}
--- Conjugator/ConjugatorLibrary/ImparfaitConjugator.cs
+++ Conjugator/ConjugatorLibrary/ImparfaitConjugator.cs
@@ -10,67 +10,37 @@ namespace ConjugatorLibrary
             Contract.Requires(verb.EndsWith("er"));
             Contract.Requires(verb.Length > 2);
 
-            if (verb == "aller")
-            {
-                return new[] {"allais", "allais", "allait", "allions", "alliez", "allaient" };
-            }
+            string stem = GetStem(verb);
 
             string[] endings = {"ais", "ais", "ait", "ions", "iez", "aient"};
             if (verb[^3] == 'g')
             {
-                endings = new[]
-                {
-                    "e" + endings[0],
-                    "e" + endings[1],
-                    "e" + endings[2],
-                    endings[3],
-                    endings[4],
-                    "e" + endings[5],
-                };
+                endings = endings.ForNonNousVous(s => "e" + s);
             }
 
-            string[] withEndings = ApplyEndings(endings, verb);
+            string[] withEndings = AddEndings(endings, stem);
 
-            // soften the c with a cedilla before an 'o' for nous (at index 3)
+            // soften the c with a cedilla before an 'a'
             if (verb[^3] == 'c')
             {
-                withEndings[0] = withEndings[0].ReplaceAt(-4, 'ç');
-                withEndings[1] = withEndings[1].ReplaceAt(-4, 'ç');
-                withEndings[2] = withEndings[2].ReplaceAt(-4, 'ç');
-                //withEndings[3] = withEndings[3].ReplaceAt(-4, 'ç');
-                //withEndings[4] = withEndings[4].ReplaceAt(-4, 'ç');
-                withEndings[5] = withEndings[5].ReplaceAt(-6, 'ç');
+                withEndings = withEndings.ForNonNousVous(s => s.ReplaceAt(stem.Length - 1, 'ç'));
             }
 
             return withEndings;
         }
 
-        private static string[] ApplyEndings(string[] endings, string verb)
+        private static string GetStem(string verb)
         {
-            string stem = verb.Remove(verb.Length - 2);
-
-            // determine the stem for je, tu, il, ils ("modifiedStem") which may
-            // not be the same as for nous and vous
-
-            return AddEndings(endings, stem);
-        }
-
-        private static string[] AddEndings(string[] endings, string modifiedStem, string nousVousStem)
-        {
-            return new[]
+            if (verb == "aller")
             {
-                modifiedStem + endings[0],
-                modifiedStem + endings[1],
-                modifiedStem + endings[2],
-                nousVousStem + endings[3],
-                nousVousStem + endings[4],
-                modifiedStem + endings[5],
-            };
+                return "all";
+            }
+            return verb.Remove(verb.Length - 2);
         }
 
-        private static string[] AddEndings(string[] endings, string modifiedStem)
+        private static string[] AddEndings(string[] endings, string stem)
         {
-            return endings.Select(ending => modifiedStem + ending).ToArray();
+            return endings.Select(ending => stem + ending).ToArray();
         }
     }
 }
