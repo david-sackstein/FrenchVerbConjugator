3891773e4918ec8efd63a5fd5f43a0e5418cbd31
David Sackstein <davids@codeprecise.com>
2020-10-20 01:16:28 +0300

    Fixed all errors

--- Conjugator/ConjugatorLibrary/Conjugators/SubjonctifImparfaitConjugator.cs
+++ Conjugator/ConjugatorLibrary/Conjugators/SubjonctifImparfaitConjugator.cs
@@ -7,12 +7,20 @@ namespace ConjugatorLibrary
         public static string[] GetConjugations(string verb)
         {
             var stem = verb.Remove(verb.Length - 2);
+            
+            // soften the c with a cedilla before the 'a'
+            if (verb[^3] == 'c')
+            {
+                stem = stem.ReplaceAt(-1, 'ç');
+            }
+            
             var endings = new[] {"asse", "asses", "ât", "assions", "assiez", "assent" };
             // soften the g before the 'a' by adding an e
             if (verb[^3] == 'g')
             {
                 endings = endings.Select(ending => "e" + ending).ToArray();
             }
+
             return stem.AddEndings(endings);
         }
     }
