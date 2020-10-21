9fe915910d58686d56eae6784816a7abff232c23
David Sackstein <davids@codeprecise.com>
2020-10-21 22:39:12 +0300

    41 errors

--- Conjugator/ConjugatorLibrary/SecondGroup/PresentConjugator.cs
+++ Conjugator/ConjugatorLibrary/SecondGroup/PresentConjugator.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using System.Linq;
 
 namespace ConjugatorLibrary.SecondGroup
 {
-    static class PresentConjugator
+    internal static class PresentConjugator
     {
-        private static readonly string[] Endings = {"is", "is" , "it", "issons", "issez", "issent" };
+        private static readonly string[] Endings = {"is", "is", "it", "issons", "issez", "issent"};
 
         public static string[] GetConjugations(string verb)
         {
@@ -13,24 +13,24 @@ namespace ConjugatorLibrary.SecondGroup
                 return new[] {"ai", "as", "a", "avons", "avez", "ont"};
             }
 
-            var stem = verb[..^2];
+            string stem = verb[..^2];
 
             if (verb.EndsWith("enir"))
             {
                 string modifiedStem = stem.TrimEnd("en") + "ien";
-                var endings = new[] {"s", "s", "t" , "ons" , "ez" , "nent" };
+                var endings = new[] {"s", "s", "t", "ons", "ez", "nent"};
                 return endings.AddEndings(modifiedStem, stem);
             }
 
             if (verb.EndsWith("courir"))
             {
-                var endings = new[] { "s", "s", "t", "ons", "ez", "ent" };
+                var endings = new[] {"s", "s", "t", "ons", "ez", "ent"};
                 return endings.AddEndings(stem);
             }
 
             if (verb.EndsWith("cueillir"))
             {
-                var endings = new[] { "e", "es", "e", "ons", "ez", "ent" };
+                var endings = new[] {"e", "es", "e", "ons", "ez", "ent"};
                 return endings.AddEndings(stem);
             }
 
@@ -38,43 +38,50 @@ namespace ConjugatorLibrary.SecondGroup
             {
                 string modifiedStem = stem.TrimEnd("ér") + "ier";
                 string ilsStem = stem.TrimEnd("ér") + "ièr";
-                var endings = new[] { "s", "s", "t", "ons", "ez", "ent" };
-                var modified = endings.AddEndings(modifiedStem, stem, ilsStem);
+                var endings = new[] {"s", "s", "t", "ons", "ez", "ent"};
+                string[] modified = endings.AddEndings(modifiedStem, stem, ilsStem);
                 return modified;
             }
 
             if (verb.EndsWith("cevoir"))
             {
-                var endings = new[] { "çois", "çois", "çoit", "cevons", "cevez", "çoivent" };
-                var modified = endings.AddEndings(verb.TrimEnd("cevoir"));
+                var endings = new[] {"çois", "çois", "çoit", "cevons", "cevez", "çoivent"};
+                string[] modified = endings.AddEndings(verb.TrimEnd("cevoir"));
                 return modified;
             }
 
             if (verb.EndsWith("voir") || verb.EndsWith("choir"))
             {
-                var endings = new[] { "is", "is", "it", "yons", "yez", "ient" };
-                var modified = endings.AddEndings(stem);
+                var endings = new[] {"is", "is", "it", "yons", "yez", "ient"};
+                string[] modified = endings.AddEndings(stem);
                 return modified;
             }
 
             if (verb.EndsWith("valoir"))
             {
-                var endings = new[] { "aux", "aux", "aut", "alons", "alez", "alent" };
-                var modified = endings.AddEndings(verb.TrimEnd("aloir"));
+                var endings = new[] {"aux", "aux", "aut", "alons", "alez", "alent"};
+                string[] modified = endings.AddEndings(verb.TrimEnd("aloir"));
                 return modified;
             }
 
             if (verb.EndsWith("aillir") && !verb.EndsWith("jaillir") || verb.EndsWith("ouvrir"))
             {
-                var endings = new[] { "e", "es", "e", "ons", "ez", "ent" };
-                var modified = endings.AddEndings(stem);
+                var endings = new[] {"e", "es", "e", "ons", "ez", "ent"};
+                string[] modified = endings.AddEndings(stem);
                 return modified;
             }
 
             if (verb.EndsWith("sentir"))
             {
-                var endings = new[] { "s", "s", "t", "tons", "tez", "tent" };
-                var modified = endings.AddEndings(verb.TrimEnd("tir"));
+                var endings = new[] {"s", "s", "t", "tons", "tez", "tent"};
+                string[] modified = endings.AddEndings(verb.TrimEnd("tir"));
+                return modified;
+            }
+
+            if (verb.EndsWith("ervir") && !new[] {"asservir", "réasservir"}.Contains(verb))
+            {
+                var endings = new[] {"s", "s", "t", "vons", "vez", "vent"};
+                string[] modified = endings.AddEndings(verb.TrimEnd("vir"));
                 return modified;
             }
 
