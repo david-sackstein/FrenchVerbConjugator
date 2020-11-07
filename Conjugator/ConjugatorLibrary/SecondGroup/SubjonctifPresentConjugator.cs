ff925323708e9be03778c3d1eb8f0f9dba626179
David Sackstein <davids@codeprecise.com>
2020-11-07 20:41:47 +0200

    refactoring

--- Conjugator/ConjugatorLibrary/SecondGroup/SubjonctifPresentConjugator.cs
+++ Conjugator/ConjugatorLibrary/SecondGroup/SubjonctifPresentConjugator.cs
@@ -4,43 +4,22 @@ namespace ConjugatorLibrary.SecondGroup
 {
     public static class SubjonctifPresentConjugator
     {
+        private static readonly string[] Endings = { "e", "es", "e", "ions", "iez", "ent" };
+
         public static string[] GetConjugations(string verb)
         {
-            if (verb == "avoir")
+            switch (verb)
             {
-                return new[] {"aie", "aies", "ait", "ayons", "ayez", "aient"};
-            }
-
-            string[] endings = {"e", "es", "e", "ions", "iez", "ent"};
-
-            return ApplyEndings(endings, verb);
-        }
-
-        private static string[] ApplyEndings(string[] endings, string verb)
-        {
-            var present = PresentConjugator.GetConjugations(verb);
-            var nousForm = present[3];
-
-            if (verb == "savoir")
-            {
-                return new[] {"sache", "saches", "sache", "sachions", "sachiez", "sachent"};
-            }
-
-            if (verb == "falloir")
-            {
-                return new[] {"", "", "faille", "", "", ""};
-            }
-
-            if (verb == "pouvoir")
-            {
-                var stem = "puiss";
-                return AddEndings(endings, stem);
-            }
-
-            if (verb == "pleuvoir")
-            {
-                var stem = verb.TrimEnd("oir");
-                return AddEndings(endings, stem);
+                case "avoir":
+                    return new[] {"aie", "aies", "ait", "ayons", "ayez", "aient"};
+                case "falloir":
+                    return new[] { "", "", "faille", "", "", "" };
+                case "savoir":
+                    return Endings.AddEndings("sach");
+                case "pouvoir":
+                    return Endings.AddEndings("puiss");
+                case "pleuvoir":
+                    return Endings.AddEndings("pleuv");
             }
 
             if (verb.EndsWith("seoir"))
@@ -48,68 +27,72 @@ namespace ConjugatorLibrary.SecondGroup
                 var stem = verb.TrimEnd("eoir");
                 if (verb.EndsWith("asseoir"))
                 {
-                    var nousVousStem = stem + "oy";
-                    var nonNousVousStem = stem + "oi";
-                    return AddEndings(endings, nonNousVousStem, nousVousStem);
+                    return AddEndings(Endings, new ComplexStem(stem + "oi", stem + "oy"));
                 }
 
-                var modifiedStem = stem + "ié";
-                return AddEndings(endings, modifiedStem);
+                return Endings.AddEndings(stem + "ié");
             }
 
+            ComplexStem complexStem = GetComplexStem(verb);
+            if (complexStem != null)
+            {
+                return AddEndings(Endings, complexStem);
+            }
+
+            var present = PresentConjugator.GetConjugations(verb);
+            var nousForm = present[3];
+
+            var regularStem = nousForm.TrimEnd("ons");
+            return Endings.AddEndings(regularStem);
+        }
+
+        private static ComplexStem GetComplexStem(string verb)
+        {
             if (verb.EndsWith("enir"))
             {
                 var stem = verb.TrimEnd("enir");
-                var nousVousStem = stem + "en";
-                var nonNousVousStem = stem + "ienn";
-                return AddEndings(endings, nonNousVousStem, nousVousStem);
+                return new ComplexStem(stem + "ienn", stem + "en");
             }
 
             if (verb.EndsWith("quérir"))
             {
                 var stem = verb.TrimEnd("érir");
-                var nousVousStem = stem + "ér";
-                var nonNousVousStem = stem + "ièr";
-                return AddEndings(endings, nonNousVousStem, nousVousStem);
+                return new ComplexStem(stem + "ièr", stem + "ér");
             }
 
             if (verb == "vouloir")
             {
-                var nousVousStem = "voul";
-                var nonNousVousStem = "veuill";
-                return AddEndings(endings, nonNousVousStem, nousVousStem);
+                return new ComplexStem("veuill", "voul");
             }
 
-            if (verb.EndsWith("cevoir"))
+            if (verb.EndsWith("aloir") && verb != "prévaloir")
+            {
+                var stem = verb.TrimEnd("loir");
+                return new ComplexStem(stem + "ill", stem + "l");
+            }
+
+            if (Exceptions.cevoirVerbs.Contains(verb))
             {
                 var stem = verb.TrimEnd("cevoir");
-                var nousVousStem = stem + "cev";
-                var nonNousVousStem = stem + "çoiv";
-                return AddEndings(endings, nonNousVousStem, nousVousStem);
+                return new ComplexStem(stem + "çoiv", stem + "cev");
             }
 
-            if (verb.EndsWith("devoir"))
+            if (Exceptions.devoirVerbs.Contains(verb))
             {
                 var stem = verb.TrimEnd("devoir");
-                var nousVousStem = stem + "dev";
-                var nonNousVousStem = stem + "doiv";
-                return AddEndings(endings, nonNousVousStem, nousVousStem);
+                return new ComplexStem(stem + "doiv", stem + "dev");
             }
 
             if (verb.EndsWith("mouvoir"))
             {
                 var stem = verb.TrimEnd("mouvoir");
-                var nousVousStem = stem + "mouv";
-                var nonNousVousStem = stem + "meuv";
-                return AddEndings(endings, nonNousVousStem, nousVousStem);
+                return new ComplexStem(stem + "meuv", stem + "mouv");
             }
 
             if (verb == "mourir")
             {
                 var stem = verb.TrimEnd("mourir");
-                var nousVousStem = "mour";
-                var nonNousVousStem = "meur";
-                return AddEndings(endings, nonNousVousStem, nousVousStem);
+                return new ComplexStem(stem + "meur", stem + "mour");
             }
 
             if (verb.EndsWith("fuir") ||
@@ -117,31 +100,17 @@ namespace ConjugatorLibrary.SecondGroup
                 verb.EndsWith("voir"))
             {
                 var stem = verb.TrimEnd("ir");
-                var nousVousStem = stem + "y";
-                var nonNousVousStem = stem + "i";
-                return AddEndings(endings, nonNousVousStem, nousVousStem);
+                return new ComplexStem(stem + "i", stem + "y");
             }
 
-            if (verb.EndsWith("aloir") && verb != "prévaloir")
-            {
-                var stem = verb.TrimEnd("loir");
-                var nousVousStem = stem + "l";
-                var nonNousVousStem = stem + "ill";
-                return AddEndings(endings, nonNousVousStem, nousVousStem);
-            }
-
-            var regularStem = nousForm.TrimEnd("ons");
-            return AddEndings(endings, regularStem);
+            return null;
         }
 
-        private static string[] AddEndings(string[] endings, string stem, string nousVousStem)
+        private static string[] AddEndings(string[] endings, ComplexStem complexStem)
         {
-            return endings.MatchNousVous(s => stem + s, s => nousVousStem + s);
-        }
-
-        private static string[] AddEndings(string[] endings, string stem)
-        {
-            return endings.Select(ending => stem + ending).ToArray();
+            return endings.MatchNousVous(
+                ending => complexStem.NonNousVousStem + ending, 
+                ending => complexStem.NousVousStem + ending);
         }
     }
 }
