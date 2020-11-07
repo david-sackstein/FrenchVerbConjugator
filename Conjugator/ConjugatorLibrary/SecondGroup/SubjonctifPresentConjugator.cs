decd3a9843e9b2fb10a5f7d662abfc8cc70373c7
David Sackstein <davids@codeprecise.com>
2020-11-07 20:00:14 +0200

    Refactoring

--- Conjugator/ConjugatorLibrary/SecondGroup/SubjonctifPresentConjugator.cs
+++ Conjugator/ConjugatorLibrary/SecondGroup/SubjonctifPresentConjugator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace ConjugatorLibrary.SecondGroup
 {
@@ -7,7 +6,10 @@ namespace ConjugatorLibrary.SecondGroup
     {
         public static string[] GetConjugations(string verb)
         {
-            if (verb == "avoir") return new[] {"aie", "aies", "ait", "ayons", "ayez", "aient"};
+            if (verb == "avoir")
+            {
+                return new[] {"aie", "aies", "ait", "ayons", "ayez", "aient"};
+            }
 
             string[] endings = {"e", "es", "e", "ions", "iez", "ent"};
 
@@ -16,157 +18,120 @@ namespace ConjugatorLibrary.SecondGroup
 
         private static string[] ApplyEndings(string[] endings, string verb)
         {
-            try
+            var present = PresentConjugator.GetConjugations(verb);
+            var nousForm = present[3];
+
+            if (verb == "savoir")
             {
-                var present = PresentConjugator.GetConjugations(verb);
-                var nousForm = present[3];
+                return new[] {"sache", "saches", "sache", "sachions", "sachiez", "sachent"};
+            }
 
-                if (verb == "savoir")
-                {
-                    return new[] {"sache", "saches", "sache", "sachions", "sachiez", "sachent"};
-                }
+            if (verb == "falloir")
+            {
+                return new[] {"", "", "faille", "", "", ""};
+            }
 
-                if (verb.EndsWith("enir"))
-                {
-                    var stem = verb.TrimEnd("enir");
-                    var nousVousStem = stem + "en";
-                    var nonNousVousStem = stem + "ienn";
-                    return AddEndings(endings, nonNousVousStem, nousVousStem);
-                }
+            if (verb == "pouvoir")
+            {
+                var stem = "puiss";
+                return AddEndings(endings, stem);
+            }
 
-                if (verb.EndsWith("quérir"))
-                {
-                    var stem = verb.TrimEnd("érir");
-                    var nousVousStem = stem + "ér";
-                    var nonNousVousStem = stem + "ièr";
-                    return AddEndings(endings, nonNousVousStem, nousVousStem);
-                }
+            if (verb == "pleuvoir")
+            {
+                var stem = verb.TrimEnd("oir");
+                return AddEndings(endings, stem);
+            }
 
-                if (verb == "falloir")
-                {
-                    return new[] { "", "", "faille", "", "", "" };
-                }
-                if (verb == "vouloir")
-                {
-                    var nousVousStem = "voul";
-                    var nonNousVousStem = "veuill";
-                    return AddEndings(endings, nonNousVousStem, nousVousStem);
-                }
-
-                if (verb.EndsWith("cevoir"))
-                {
-                    var stem = verb.TrimEnd("cevoir");
-                    var nousVousStem = stem + "cev";
-                    var nonNousVousStem = stem + "çoiv";
-                    return AddEndings(endings, nonNousVousStem, nousVousStem);
-                }
-
-                if (verb.EndsWith("devoir"))
-                {
-                    var stem = verb.TrimEnd("devoir");
-                    var nousVousStem = stem + "dev";
-                    var nonNousVousStem = stem + "doiv";
-                    return AddEndings(endings, nonNousVousStem, nousVousStem);
-                }
-
-                if (verb.EndsWith("fuir"))
-                {
-                    var stem = verb.TrimEnd("fuir");
-                    var nousVousStem = stem + "fuy";
-                    var nonNousVousStem = stem + "fui";
-                    return AddEndings(endings, nonNousVousStem, nousVousStem);
-                }
-
-                if (verb.EndsWith("choir"))
-                {
-                    var stem = verb.TrimEnd("ir");
-                    var nousVousStem = stem + "y";
-                    var nonNousVousStem = stem + "i";
-                    return AddEndings(endings, nonNousVousStem, nousVousStem);
-                }
-
-                if (verb == "mourir")
-                {
-                    var stem = verb.TrimEnd("mourir");
-                    var nousVousStem = "mour";
-                    var nonNousVousStem = "meur";
-                    return AddEndings(endings, nonNousVousStem, nousVousStem);
-                }
-
-                if (verb.EndsWith("mouvoir"))
-                {
-                    var stem = verb.TrimEnd("mouvoir");
-                    var nousVousStem = stem + "mouv";
-                    var nonNousVousStem = stem + "meuv";
-                    return AddEndings(endings, nonNousVousStem, nousVousStem);
-                }
-
-                if (verb.EndsWith("aloir") && verb != "prévaloir")
-                {
-                    var stem = verb.TrimEnd("loir");
-                    var nousVousStem = stem + "l";
-                    var nonNousVousStem = stem + "ill";
-                    return AddEndings(endings, nonNousVousStem, nousVousStem);
-                }
-
-                if (verb == "pleuvoir")
-                {
-                    //"émouvoir",
-                    //"émeuve              émouve",
-                    //"émeuves             émouves",
-                    //"émeuve              émouve",
-                    //"émouvions           émouvions",
-                    //"émouviez            émouviez",
-                    //"émeuvent            émouvent"
-                    var stem = verb.TrimEnd("oir");
-                    return AddEndings(endings, stem);
-                }
-
+            if (verb.EndsWith("seoir"))
+            {
+                var stem = verb.TrimEnd("eoir");
                 if (verb.EndsWith("asseoir"))
                 {
-                    var stem = verb.TrimEnd("eoir");
                     var nousVousStem = stem + "oy";
                     var nonNousVousStem = stem + "oi";
                     return AddEndings(endings, nonNousVousStem, nousVousStem);
                 }
 
-                if (verb.EndsWith("seoir"))
-                {
-                    var stem = verb.TrimEnd("seoir") + "sié";
-                    return AddEndings(endings, stem);
-                }
-
-                if (verb == "pouvoir")
-                {
-                    var stem = "puiss";
-                    return AddEndings(endings, stem);
-                }
-
-                if (verb == "voir" || verb == "revoir")
-                {
-                    var stem = verb.TrimEnd("ir");
-                    var nousVousStem = stem + "y";
-                    var nonNousVousStem = stem + "i";
-                    return AddEndings(endings, nonNousVousStem, nousVousStem);
-                }
-
-                if (verb.EndsWith("pourvoir") || verb == "entrevoir" || verb == "prévoir")
-                {
-                    var stem = verb.TrimEnd("ir");
-                    var nousVousStem = stem + "y";
-                    var nonNousVousStem = stem + "i";
-                    return AddEndings(endings, nonNousVousStem, nousVousStem);
-                }
-                else
-                {
-                    var stem = nousForm.TrimEnd("ons");
-                    return AddEndings(endings, stem);
-                }
+                var modifiedStem = stem + "ié";
+                return AddEndings(endings, modifiedStem);
             }
-            catch (Exception ex)
+
+            if (verb.EndsWith("enir"))
             {
-                return AddEndings(endings, verb.TrimEnd("ir"));
+                var stem = verb.TrimEnd("enir");
+                var nousVousStem = stem + "en";
+                var nonNousVousStem = stem + "ienn";
+                return AddEndings(endings, nonNousVousStem, nousVousStem);
             }
+
+            if (verb.EndsWith("quérir"))
+            {
+                var stem = verb.TrimEnd("érir");
+                var nousVousStem = stem + "ér";
+                var nonNousVousStem = stem + "ièr";
+                return AddEndings(endings, nonNousVousStem, nousVousStem);
+            }
+
+            if (verb == "vouloir")
+            {
+                var nousVousStem = "voul";
+                var nonNousVousStem = "veuill";
+                return AddEndings(endings, nonNousVousStem, nousVousStem);
+            }
+
+            if (verb.EndsWith("cevoir"))
+            {
+                var stem = verb.TrimEnd("cevoir");
+                var nousVousStem = stem + "cev";
+                var nonNousVousStem = stem + "çoiv";
+                return AddEndings(endings, nonNousVousStem, nousVousStem);
+            }
+
+            if (verb.EndsWith("devoir"))
+            {
+                var stem = verb.TrimEnd("devoir");
+                var nousVousStem = stem + "dev";
+                var nonNousVousStem = stem + "doiv";
+                return AddEndings(endings, nonNousVousStem, nousVousStem);
+            }
+
+            if (verb.EndsWith("mouvoir"))
+            {
+                var stem = verb.TrimEnd("mouvoir");
+                var nousVousStem = stem + "mouv";
+                var nonNousVousStem = stem + "meuv";
+                return AddEndings(endings, nonNousVousStem, nousVousStem);
+            }
+
+            if (verb == "mourir")
+            {
+                var stem = verb.TrimEnd("mourir");
+                var nousVousStem = "mour";
+                var nonNousVousStem = "meur";
+                return AddEndings(endings, nonNousVousStem, nousVousStem);
+            }
+
+            if (verb.EndsWith("fuir") ||
+                verb.EndsWith("choir") ||
+                verb.EndsWith("voir"))
+            {
+                var stem = verb.TrimEnd("ir");
+                var nousVousStem = stem + "y";
+                var nonNousVousStem = stem + "i";
+                return AddEndings(endings, nonNousVousStem, nousVousStem);
+            }
+
+            if (verb.EndsWith("aloir") && verb != "prévaloir")
+            {
+                var stem = verb.TrimEnd("loir");
+                var nousVousStem = stem + "l";
+                var nonNousVousStem = stem + "ill";
+                return AddEndings(endings, nonNousVousStem, nousVousStem);
+            }
+
+            var regularStem = nousForm.TrimEnd("ons");
+            return AddEndings(endings, regularStem);
         }
 
         private static string[] AddEndings(string[] endings, string stem, string nousVousStem)
