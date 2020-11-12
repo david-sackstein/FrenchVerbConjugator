67ebee3785f68498858189e82215f1260869b4ab
David Sackstein <davids@codeprecise.com>
2020-11-12 03:09:11 +0200

    refactoring

--- Conjugator/ConjugatorLibrary/SecondGroup/SubjonctifImparfaitConjugator.cs
+++ Conjugator/ConjugatorLibrary/SecondGroup/SubjonctifImparfaitConjugator.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Data;
-using System.Linq;
+﻿using System.Linq;
 
 namespace ConjugatorLibrary.SecondGroup
 {
     public static class SubjonctifImparfaitConjugator
     {
-        private static readonly string[] Endings = {"isse", "isses", "ît", "issions", "issiez", "issent"};
-
+        private static readonly string[] issEndings = {"isse", "isses", "ît", "issions", "issiez", "issent"};
+        private static readonly string[] ussEndings = { "usse", "usses", "ût", "ussions", "ussiez", "ussent" };
+        
         public static string[] GetConjugations(string verb)
         {
-            var uEndings = new[] { "usse", "usses", "ût", "ussions", "ussiez", "ussent" };
-
             if (verb == "pleuvoir")
             {
                 return new[] {"", "", "plût", "", "", ""};
@@ -19,77 +16,77 @@ namespace ConjugatorLibrary.SecondGroup
 
             if (verb == "avoir")
             {
-                return uEndings.AddEndings("e");
+                return ussEndings.AddEndings("e");
             }
 
             if (verb == "savoir")
             {
-                return uEndings.AddEndings("s");
+                return ussEndings.AddEndings("s");
             }
 
             if (verb.EndsWith("enir"))
             {
-                var modifiedStem = verb.TrimEnd("enir");
-                var modifiedEndings = Endings.Select(s => s.Insert(1, "n")).ToArray();
-                return modifiedEndings.AddEndings(modifiedStem);
+                var stem = verb.TrimEnd("enir");
+                var modifiedEndings = issEndings.Select(s => s.Insert(1, "n")).ToArray();
+                return modifiedEndings.AddEndings(stem);
             }
 
             if (verb.EndsWith("ourir"))
             {
                 string stem = verb.Remove(verb.Length - 2);
-                return uEndings.AddEndings(stem);
+                return ussEndings.AddEndings(stem);
             }
 
             if (Exceptions.cevoirVerbs.Contains(verb))
             {
                 string modifiedStem = verb.TrimEnd("cevoir") + "ç";
-                return uEndings.AddEndings(modifiedStem);
+                return ussEndings.AddEndings(modifiedStem);
             }
 
             if (verb.EndsWith("quérir"))
             {
                 string stem = verb.TrimEnd("érir");
-                return Endings.AddEndings(stem);
+                return issEndings.AddEndings(stem);
             }
 
             if (verb.EndsWith("eoir"))
             {
                 string stem = verb.TrimEnd("eoir");
-                return Endings.AddEndings(stem);
+                return issEndings.AddEndings(stem);
             }
 
             if (verb.EndsWith("ouvoir"))
             {
                 string stem = verb.TrimEnd("ouvoir");
-                return uEndings.AddEndings(stem);
+                return ussEndings.AddEndings(stem);
             }
 
             if (verb.EndsWith("revoir"))
             {
                 string modifiedStem = verb.TrimEnd("oir");
-                return Endings.AddEndings(modifiedStem);
+                return issEndings.AddEndings(modifiedStem);
             }
 
             if (verb.EndsWith("devoir"))
             {
                 string modifiedStem = verb.TrimEnd("evoir");
-                return uEndings.AddEndings(modifiedStem);
+                return ussEndings.AddEndings(modifiedStem);
             }
 
             if (verb == "prévoir" || verb == "voir")
             {
                 string modifiedStem = verb.TrimEnd("oir");
-                return Endings.AddEndings(modifiedStem);
+                return issEndings.AddEndings(modifiedStem);
             }
 
             if (verb.EndsWith("oir"))
             {
                 string modifiedStem = verb.TrimEnd("oir");
-                return uEndings.AddEndings(modifiedStem);
+                return ussEndings.AddEndings(modifiedStem);
             }
 
             string regularStem = verb.Remove(verb.Length - 2);
-            return Endings.AddEndings(regularStem);
+            return issEndings.AddEndings(regularStem);
         }
     }
 }
